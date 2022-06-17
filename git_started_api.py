f12996089cc03abbd5cf4ed2b2caf2263e1cf692
Arjay Jean <thearjayjean@gmail.com>
2022-06-16 20:23:53 -0400

    Added the "Typer" library in order to use the command line with the script.

--- git_started_api.py
+++ git_started_api.py
@@ -1,50 +1,71 @@
 import subprocess
 import requests
 import json
+import typer
 
 subprocess.run('clear')
 
-repo = input(f'Name the repository: ').lower() 
-project_type = input(f'What kind of project are you creating? ').lower()
-new_directory = f'FILENAME/{repo}'
+app = typer.Typer()
 
-URL = f'https://api.github.com/user/repos'
-TOKEN = '' # PERSONAL ACCESS TOKEN
-AUTHENTICATION = f"token {TOKEN}"
+def repo(name):
+    return name
+def project(type_of):
+    return type_of
 
-jsonPayload = {"name": repo}
+def create(repo, project_type):
+    new_directory = f'FILENAME/{repo}'
 
-headers = {"Accept": "application/vnd.github.v3+json",
-            "Authorization": AUTHENTICATION      
-            }
+    URL = f'https://api.github.com/user/repos'
+    TOKEN = '' # PERSONAL ACCESS TOKEN
+    AUTHENTICATION = f"token {TOKEN}"
 
-response = requests.post(URL, headers=headers, data=json.dumps(jsonPayload))
-subprocess.run('clear')
+    jsonPayload = {"name": repo}
 
-commands_py = [
-    ['mkdir',f'{new_directory}'],
-    ['touch',f'{new_directory}/{repo}.py'],
-    ['python3', '-m', 'venv', f'{new_directory}/venv'],
-    ['git', 'init',f'{new_directory}'],
-    ['code', f'{new_directory}/'],
-    'clear'
-    ]
+    headers = {"Accept": "application/vnd.github.v3+json",
+                "Authorization": AUTHENTICATION      
+                }
 
-commands_web_dev = [
-    ['mkdir',f'{new_directory}'],
-    ['touch',f'{new_directory}/index.html'],
-    ['touch',f'{new_directory}/index.js'],
-    ['touch',f'{new_directory}/style.css'],
-    ['git', 'init',f'{new_directory}'],
-    ['code', f'{new_directory}/'],
-    'clear'
-    ]
+    response = requests.post(URL, headers=headers, data=json.dumps(jsonPayload))
 
-if project_type == 'py': 
-    project_creator = [subprocess.run(command) for command in commands_py]
-elif project_type == 'wd': 
-    project_creator = [subprocess.run(command) for command in commands_web_dev]
+    commands_py = [
+        ['mkdir',f'{new_directory}'],
+        ['touch',f'{new_directory}/{repo}.py'],
+        ['python3', '-m', 'venv', f'{new_directory}/venv'],
+        ['git', 'init',f'{new_directory}'],
+        ['code', f'{new_directory}/'],
+        'clear'
+        ]
+
+    commands_web_dev = [
+        ['mkdir',f'{new_directory}'],
+        ['touch',f'{new_directory}/index.html'],
+        ['touch',f'{new_directory}/index.js'],
+        ['touch',f'{new_directory}/style.css'],
+        ['git', 'init',f'{new_directory}'],
+        ['code', f'{new_directory}/'],
+        'clear'
+        ]
+
+    if project_type == 'py': 
+        project_creator = [subprocess.run(command) for command in commands_py]
+    elif project_type == 'wd': 
+        project_creator = [subprocess.run(command) for command in commands_web_dev]
+    subprocess.run('clear')
 
 
-if response.status_code == 201:
-    print(f"'{repo}' was successfully created!!!")
+    if response.status_code == 201:
+        print(f"'{repo}' was successfully created!!!")
+    else:
+        print(f'\n{response.status_code}')
+        print(response.content)
+
+@app.command()
+def py(name: str):
+    create(repo(name), project('py'))
+
+@app.command()
+def wd(name: str):
+    create(repo(name), project('wd'))
+
+if __name__ == "__main__":
+    app()
